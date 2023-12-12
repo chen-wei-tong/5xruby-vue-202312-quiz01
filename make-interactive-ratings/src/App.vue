36541d0c4d7435002b72b17922a7a9c717788a84
miffy <tongtongc6@gmail.com>
2023-12-12 12:06:24 +0800

    2023.12.12,upd,make-interactive-ratings,by weitong

--- make-interactive-ratings/src/App.vue
+++ make-interactive-ratings/src/App.vue
@@ -4,129 +4,100 @@
 1. 將 movies 的內容用 v-for 改寫成動態渲染
 2. 完成電影星星評分的事件綁定，同樣用 v-for 進行改寫
 */
-import { ref } from "vue";
+import { ref, reactive, computed, onMounted } from "vue";
 import { StarIcon } from "@heroicons/vue/24/solid";
 import { items } from "./movies.json";
 
 const movies = ref(items);
+// const genres = reactive({});
+// const rating = ref(0);
+
+const rating = computed(() => ratingHover.value = stars.rating);
+
+const ratingHover = ref(null);
+
+const ratingToggle = (value) => {
+  stars.rating = value;
+  console.log(`評等：${stars.rating}`);
+};
+const stars = reactive({
+  rating: "",
+});
+const setHoverRating = (value) => {
+  ratingHover.value = value;
+};
+const resetHoverRating = () => {
+  ratingHover.value = null;
+};
+onMounted(() => {
+  if (items && items.length > 0) {
+    for (let i = 0; i < items.length; i++) {
+      console.log(`Movie ${i + 1} Rating: ${items[i].rating}`);
+      console.log("41", items[i].rating);
+    }
+    items.forEach((movie, index) => {
+      for (let i = 0; i < items.length; i++) {
+        if (items && items.length > 0) {
+          console.log("items48", items[i].rating);
+        }
+      }
+    });
+  }
+});
 </script>
 
 <template>
-<div class="app">
-  <!--v-for?star狀態-->
-  <div class="movie-list">
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UY1982_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Godfather</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Crime</span>
-            <span class="movie-item-genre-tag">Drama</span>
+  <div class="app">
+    <div class="movie-list">
+      <div class="movie-item" v-for="movie in movies" :key="movie.id">
+        <div class="movie-item-image-wrapper">
+          <img :src="movie.image" class="movie-item-image" alt="movie" />
+        </div>
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">
+              {{ movie.name }}
+            </h3>
+            <div class="movie-item-genres-wrapper">
+              <span
+                v-for="(genre, index) in movie.genres"
+                :key="index"
+                class="movie-item-genre-tag"
+              >
+                {{ genre }}
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (4/5) </span>
-          
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">
+              {{ movie.description }}
+            </p>
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1200_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Shawshank Redemption</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Drama</span>
-          </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (4/5) </span>
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="movie-item">
-      <div class="movie-item-image-wrapper">
-        <img src="https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UY2048_.jpg" class="movie-item-image" alt="">
-      </div>
-      <div class="movie-item-content-wrapper">
-        <div class="movie-item-title-wrapper">
-          <h3 class="movie-item-title">The Dark Knight</h3>
-          <div class="movie-item-genres-wrapper">
-            <span class="movie-item-genre-tag">Action</span>
-            <span class="movie-item-genre-tag">Crime</span>
-            <span class="movie-item-genre-tag">Drama</span>
-          </div>
-        </div>
-        <div class="movie-item-description-wrapper">
-          <p class="movie-item-description">When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.</p>
-        </div>
-        <div class="movie-item-rating-wrapper">
-          <span class="movie-item-rating-text"> Rating: (3/5) </span>
-          <div class="movie-item-star-icon-wrapper">
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-yellow-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
-            <button class="text-gray-500 movie-item-star-icon-button">
-              <StarIcon class="movie-item-star-icon" />
-            </button>
+          <div class="movie-item-rating-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ stars.rating }}/5)
+            </span>
+
+            <div class="movie-item-star-icon-wrapper">
+              <button
+                v-for="i in 5"
+                :key="i"
+                class="movie-item-star-icon-button"
+                :class="[
+                  i <= (ratingHover || stars.rating)
+                    ? 'text-yellow-500'
+                    : 'text-gray-500',
+                ]"
+                @mouseover="setHoverRating(i)"
+                @mouseout="resetHoverRating"
+                @click="ratingToggle(i)"
+              >
+                <StarIcon class="movie-item-star-icon" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
